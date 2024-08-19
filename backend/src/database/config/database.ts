0dceca68d104a84dc2900f5488589679db7b392e
valedobrandi <valedobrandi@gmail.com>
2024-08-19 16:24:51 -0300

    feat

--- backend/src/database/config/database.ts
+++ backend/src/database/config/database.ts
@@ -3,7 +3,7 @@ import { Options } from 'sequelize';
 const config: Options = {
   username: process.env.MYSQLUSER || 'root',
   password: process.env.MYSQLPASSWORD || '123456',
-  database: 'APPLICATION_RECIPES',
+  database: process.env.MYSQLDATABASE || 'APPLICATION_RECIPES',
   host: process.env.MYSQLHOST || 'localhost',
   port: Number(process.env.MYSQLPORT) || 3306,
   dialect: 'mysql',
