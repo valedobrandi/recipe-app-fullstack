b9d9e0d00217608766572115250464a7f64d5cc8
valedobrandi <valedobrandi@gmail.com>
2024-08-19 14:02:42 -0300

    feat database variable

--- backend/src/database/config/database.ts
+++ backend/src/database/config/database.ts
@@ -3,8 +3,8 @@ import { Options } from 'sequelize';
 const config: Options = {
   username: process.env.MYSQLUSER || 'root',
   password: process.env.MYSQLPASSWORD || '123456',
-  database: 'APPLICATION_RECIPES',
-  host: process.env.MYSQL_URL || 'localhost',
+  database: process.env.MYSQLDATABASE || 'APPLICATION_RECIPES',
+  host: process.env.MYSQLHOST || 'localhost',
   port: Number(process.env.MYSQLPORT) || 3306,
   dialect: 'mysql',
   dialectOptions: {
